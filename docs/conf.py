d061bcebaf1017b7c3eceacd83651fbb7f26107c
Dreafun <Dreafun-AA72@yandex.ru>
2026-04-07 21:52:11 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,3 +11,22 @@ source_suffix = {
     ".rst": "restructuredtext",
     ".md": "markdown",
 }
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#7c4dff",
+        "color-brand-content": "#ff4d88",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#bb86fc",
+        "color-brand-content": "#ff79c6",
+    },
+}
+
+myst_enable_extensions = ["colon_fence",
+"deflist",
+"dollarmath",
+"amsmath",
+"html_admonition",
+"html_image",
+"tasklist",]
