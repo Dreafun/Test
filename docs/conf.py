99a21de44be05d6718163f3bdb1ef47c522c9ddb
Dreafun <Dreafun-AA72@yandex.ru>
2026-04-07 21:48:16 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath("."))
 project = "Test"
 extensions = ["myst_parser", "sphinx_rtd_theme"]
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 source_suffix = {
     ".rst": "restructuredtext",
